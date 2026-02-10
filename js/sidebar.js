797a9de9b78c14114b81b4fc4bd294cd5b43128c
Hesh <henochshungu@gmail.com>
2026-02-10 22:29:41 +0200

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,9 @@
+function openSidebar() {
+  document.getElementById("sidebar").classList.remove("sidebar-hidden");
+  document.getElementById("sidebarOverlay").classList.remove("hidden");
+}
 
+function closeSidebar() {
+  document.getElementById("sidebar").classList.add("sidebar-hidden");
+  document.getElementById("sidebarOverlay").classList.add("hidden");
+}
